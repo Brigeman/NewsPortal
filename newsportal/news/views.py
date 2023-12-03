11aac7eb61fe3b008cd9cc973c8a7b77df9bc4cd
Brigeman <yurgen.brigeman@mail.ru>
2023-12-03 14:23:35 +0100

    cache

--- newsportal/news/views.py
+++ newsportal/news/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from django.urls import reverse_lazy
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 
 
 class NewsListView(ListView):
@@ -9,11 +11,19 @@ class NewsListView(ListView):
     context_object_name = 'news'
     ordering = ['-created_at']
 
+    @method_decorator(cache_page(300))  # кэширование на 5 минут (300 секунд)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
 
 class NewsDetailView(DetailView):
     model = Post
     template_name = 'news/news_detail.html'
     context_object_name = 'article'
+
+    @method_decorator(cache_page(60))  # кэширование на 1 минуту (60 секунд)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
 
 class NewsCreateView(CreateView):
--- newsportal/newsportal/settings.py
+++ newsportal/newsportal/settings.py
@@ -173,3 +173,12 @@ CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
+
+# кэш
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        'LOCATION': 'unique-snowflake',  # Это значение не имеет значения для DummyCache
+    }
+}
+
